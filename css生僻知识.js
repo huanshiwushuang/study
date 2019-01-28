05723d11b652ef4f19aaca9a7fb12f09339b5b88
guohao <1403902918@qq.com>
2019-01-28 16:36:31 +0800

    添加Linux笔记

--- css生僻知识.js
+++ css生僻知识.js
@@ -1,3 +1,20 @@
 // 1、顺序不能反，否则 background-size 不生效
 // background: url(/static/imgs/loading/loading.gif);
 // background-size: cover;
+
+// 2、弹性布局
+// container:
+//     flex-direction: row row-reverse column column-reverse
+//     flex-wrap: nowrap wwap wrap-reverse
+//     flex-flow: flex-direction flex-wrap
+//     justify-content: flex-start flex-end center space-between space-around
+//     align-items: flex-start flex-end center baseline stretch
+//     align-content: flex-start flex-end center space-between space-around stretch
+
+// item:
+//     order: 0 (integer)
+//     flex-grow: 0 (integer)
+//     flex-shrink: 1 (integer)
+//     flex-basis: auto (pixel)
+//     flex: none | auto | (flex-grow)
+//     align-self: auto flex-start flex-end center baseline stretch 
--- git操作.js
+++ git操作.js
@@ -1,3 +1,4 @@
 // git log -p --after={2018-12-29} --before={2019-01-01} --reverse --author=guohao --graph --online --no-merges
 // git log --name-only --name-status
 // git pull origin master --allow-unrelated-histories 【允许合并不相关的历史】
+// git show -stat commit-id 【查看某次提交的修改文件列表】
