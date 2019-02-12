c4cfbf1991074263f93cd540a8c29c36ab1ea3e9
guohao <1403902918@qq.com>
2019-02-12 17:09:20 +0800

    添加了 .gitignore

--- git操作.js
+++ git操作.js
@@ -2,3 +2,5 @@
 // git log --name-only --name-status
 // git pull origin master --allow-unrelated-histories 【允许合并不相关的历史】
 // git show -stat commit-id 【查看某次提交的修改文件列表】
+// git check-ignore -v App.class 【检查哪一条配置忽略了文件】
+// git add -f App.class 【-f 参数可以强行提交忽略的文件】
