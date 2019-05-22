75a9116938cc0ba2db7fd2bac3543a2421a08b77
guohao <1403902918@qq.com>
2019-05-22 19:37:14 +0800

    update

--- git操作.js
+++ git操作.js
@@ -14,3 +14,5 @@
 // git pull origin develop --allow-unrelated-histories  【允许合并不相关的历史】
 // git config --global user.email   【配置email】
 // git config --global user.name   【配置name】
+// git push origin --delete test 【删除远程 test 分支】
+// git branch -d test 【删除本地 test 分支】
